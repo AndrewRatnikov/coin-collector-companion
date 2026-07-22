0997ba0f218c2c3edc5aab8499532aa6da507ce1
Andrew <ratnikov.am@gmail.com>
2026-07-22 13:24:16 +0300

    modify: apps/web/src/app/layout.tsx
    Wire SiteNav into the root layout
    
    Run: run_20260722_121303

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { QueryProvider } from '@/components/providers/query-provider';
 import { SiteFooter } from '@/components/layout/site-footer';
+import { SiteNav } from '@/components/layout/site-nav';
 import './globals.css';
 
 const geistSans = Geist({
@@ -27,7 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <QueryProvider>{children}</QueryProvider>
+        <QueryProvider>
+          <SiteNav />
+          {children}
+        </QueryProvider>
         <SiteFooter />
       </body>
     </html>
